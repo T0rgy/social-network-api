079f7f3606a743b566166a4644724a6bcd5d5ff9
Torgy <jctorge@gmail.com>
2022-07-02 10:03:43 -0500

    server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.use(require('./routes'));
+
+// connects app to mongoDB
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+// log mongo queries being executed
+mongoose.set('debug', true);
+
+app.listen(PORT, () => console.log(`Connected on localhost: ${PORT}`));
